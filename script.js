d416c5f2849ac2cdf98b8e2b34a6db7400fb8f27
cmburch <georgia_beast89@yahoo.com>
2021-06-04 19:10:17 -0400

    add card number dynamically [WIP]

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 console.log('hello!')
 
 function onCardNumberChange() {
+  let currentValue = document.getElementById("card-number").innerHTML
   var x = document.getElementById("card-element").value;
-  console.log(x)
+  console.log(x.length)
   // document.querySelectorAll('.credit-font, .credit-card-number').innerHTML = x;
     document.getElementById("card-number").innerHTML = x;
+    if(x.length % 4 == 0){
+      // document.getElementById("card-number").innerHTML = currentValue + 99
+      document.getElementById("card-number").innerHTML += '';
+    }
 }
