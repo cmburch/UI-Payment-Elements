4b5f320f852dd5f05a433aa6be451267ba243b92
cmburch <georgia_beast89@yahoo.com>
2021-06-04 14:43:09 -0400

    [WIP] set up method to display card number

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log('hello!')
+
+function onCardNumberChange() {
+  var x = document.getElementById("card-element").value;
+  console.log(x)
+  document.getElementsByClassName("credit-card-number").innerHTML = x;
+    document.getElementById("demo").innerHTML = x;
+}
