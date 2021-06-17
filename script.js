4553f65ffcdbe341432a34ed8af27b94719dddd4
cmburch <georgia_beast89@yahoo.com>
2021-06-17 04:29:31 -0400

    add input field for the card number

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
 console.log('hello!')
 
-function onCardNumberChange() {
-  let currentValue = document.getElementById("card-number").innerHTML
-  var x = document.getElementById("card-element").value;
-  var newval = '';
-  console.log(x.length)
-  // document.querySelectorAll('.credit-font, .credit-card-number').innerHTML = x;
-    document.getElementById("card-number").innerHTML = x;
-    if(x.length % 4 == 0){
-      // document.getElementById("card-number").innerHTML = currentValue + 99
-      // document.getElementById("card-number").innerHTML += '\xa0';
 
-      for (var i = 0; i < x.length; i++) {
-        if (i % 4 == 0 && i > 0) newval = newval.concat('&nbsp');
-        newval = newval.concat(x[i]);
-      }
-      document.getElementById("card-number").innerHTML = newval
-      console.log(newval)
+function onCardNumberChange() {
+  let currentValue = document.getElementById('card-number').innerHTML;
+  var x = document.getElementsByName('cardnumber')[0].value;
+  var newval = '';
+  console.log(x.length);
+
+  document.getElementById('card-number').innerHTML = x;
+  if (x.length % 4 == 0) {
+    for (var i = 0; i < x.length; i++) {
+      if (i % 4 == 0 && i > 0) newval = newval.concat('&nbsp');
+      newval = newval.concat(x[i]);
     }
+    document.getElementById('card-number').innerHTML = newval;
+    console.log(newval);
+  }
 }
+
+// function onCardNumberChange() {
+//   let currentValue = document.getElementById("card-number").innerHTML
+//   var x = document.getElementById("card-element").value;
+//   var newval = '';
+//   console.log(x.length)
+//   // document.querySelectorAll('.credit-font, .credit-card-number').innerHTML = x;
+//     document.getElementById("card-number").innerHTML = x;
+//     if(x.length % 4 == 0){
+//       // document.getElementById("card-number").innerHTML = currentValue + 99
+//       // document.getElementById("card-number").innerHTML += '\xa0';
+
+//       for (var i = 0; i < x.length; i++) {
+//         if (i % 4 == 0 && i > 0) newval = newval.concat('&nbsp');
+//         newval = newval.concat(x[i]);
+//       }
+//       document.getElementById("card-number").innerHTML = newval
+//       console.log(newval)
+//     }
+// }
 
 // function formatCreditCard() {
 //     var x = document.getElementById("card-number");
